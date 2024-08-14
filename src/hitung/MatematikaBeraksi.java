f6bf5a7b2c703dd10e679773557a28941e8d75a4
azrasyafii <azraasyafii@gmail.com>
2024-08-15 03:20:58 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hitung;
 
-/**
- *
- * @author Pongo
- */
+
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika azra = new Matematika(8,5);
+        
+        System.out.println("Hasil Penjumlahan: "+azra.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: "+azra.setPengurangan());
+        System.out.println("Hasil Perkalian: "+azra.setPerkalian());
+        System.out.println("Hasil Pembagian: "+azra.setPembagian());
+        
+    }
 }
